261c4d9bf613a91cbb7140f0787e507f4777d6a7
Ubuntu <ubuntu@ip-172-31-17-187.ec2.internal>
2016-02-16 21:09:28 +0000

    fixed port

--- keystone.js
+++ keystone.js
@@ -12,7 +12,7 @@ var keystone = require('keystone');
 keystone.init({
 
 	'name': 'crucentralcoast.com',
-    'port' : 3003,
+    'port' : 3000,
 	'brand': 'Cru Central Coast Admin Panel',
 
 	'stylus': 'public',
